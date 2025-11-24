e9ac13bd3fe605f12648e33ac587afeaf8255e07
Christopher Dowdy <cdowdy81@gmail.com>
2025-11-24 13:40:20 -0500

    add tagging support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://cd-astro-tutorial.netlify.app"
+});
